12f1b9d3bcff6d4431b7f2fa5e0ef30b217ccc84
satkukreti <satkukreti@gmail.com>
2024-05-14 20:12:29 -0400

    finished cuda version

--- ball_sam-cuda.cu
+++ ball_sam-cuda.cu
@@ -1,23 +1,54 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <curand_kernel.h>
 
-// CUDA kernel to print "Hello, World!" from each thread
-__global__ void helloCUDA() {
-    printf("Hello, World! I'm thread %d in block %d\n", threadIdx.x, blockIdx.x);
+const int n_bins = 100;
+const int n_points = 3000;
+
+__global__ void generatePoints(int dim, int *hist, int n_bins, int n_points){
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    if (tid < n_points) {
+        curandState state;
+        curand_init(tid, 0, 0, &state); //rand num generator
+        
+        double sum_squares = 0;
+	do {
+    	sum_squares = 0.0;
+           for (int i = 0; i < dim; ++i) {
+               double p = curand_uniform(&state) * 2 - 1; //rand num between -1 and 1
+               sum_squares += p * p;
+	       if(sum_squares > 1.0)
+	       	    break;
+           }
+	} while(sum_squares > 1.0);
+
+	double distance = sqrtf(sum_squares);
+        int bin = min((int)(distance * n_bins), n_bins - 1);
+        atomicAdd(&hist[bin], 1);
+        
+    }
 }
 
 int main() {
-    // Launch the CUDA kernel with 1 block and 1 thread per block
-    helloCUDA<<<1, 1>>>();
-    
-    // Synchronize threads to ensure all kernel executions are completed
-    cudaDeviceSynchronize();
+    int *hist;
+    cudaMallocManaged(&hist, n_bins * sizeof(int));
 
-    // Check for any errors during kernel launch
-    cudaError_t cudaError = cudaGetLastError();
-    if (cudaError != cudaSuccess) {
-        printf("CUDA error: %s\n", cudaGetErrorString(cudaError));
-        return 1;
+    for (int dim = 2; dim <= 16; ++dim) {
+        cudaMemset(hist, 0, n_bins * sizeof(int));
+        int blockSize = 256;
+        int numBlocks = (n_points + blockSize - 1) / blockSize;
+        generatePoints<<<numBlocks, blockSize>>>(dim, hist, n_bins, n_points);
+        cudaDeviceSynchronize();
+
+        // Print results
+        std::cout << "Dimension: " << dim << "\n";
+        for (int i = 0; i < n_bins; ++i) {
+            std::cout << (double)(hist[i]) / n_points << " ";
+        }
+        std::cout << "\n\n";
     }
 
+    cudaFree(hist);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,11 +1,7 @@
-all: ball_sam-cuda
-
-ball_sam-cuda: ball_sam-cuda.cu
-	nvcc -O3 -o $@ $<
+all: ball_sam-cpu
 
 ball_sam-cpu: ball_sam-cpu.cpp
 	g++ -O3 -o ball_sam-cpu ball_sam-cpu.cpp
 
 clean:
 	rm ball_sam-cpu
-	rm ball_sam-cuda
